5b77f7b57a4d6dcb75c9e3836a2c67a560d47c7f
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-24 19:51:51 +0530

    Attendence

---
+++ EmployeeWageCompute/Attendence/attendence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attendence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int empInput = random.Next(0, 2);
+
+            if (isPresent==empInput)
+            {
+                Console.WriteLine("employee present");
+
+            }
+            else
+            {
+                Console.WriteLine("employee absent");
+            }
+        }
+    }
+}
